2cc9c977793aa3e8e240ef89fb12fedac09c2f5b
Evan Lavender <evanlavender@pop-os.localdomain>
2019-05-22 19:41:49 -0400

    Implemented seam construction function

---
+++ gsc.py
@@ -0,0 +1,41 @@
+def construct_seam(individual):
+    """
+    Constructs a seam from the given individual of the form:
+
+        seam = {(i, f_v(i))} from i = 1, m
+
+    Args:
+        individual: tuple consisting of pivot position and genes
+
+    Returns:
+        seam coordinates
+
+    """
+    pivot = individual[0]
+    genes = individual[1]
+
+    return [(i + 1, f_v(genes, pivot, i)) for i in range(len(genes))]
+
+
+def f_v(genes, pivot, index):
+    """
+    Function used to construct a seam from an individual
+
+    seam = {(i, f_v(i))} from i = 1, m
+
+    Args:
+        genes: list of integer values
+        pivot: pivot position
+        index: the index of the gene to transform
+
+    Returns:
+        corresponding coordinate of a gene
+
+    """
+
+    if index == pivot:
+        return genes[index]
+    elif index > pivot:
+        return genes[index] + f_v(genes, pivot, index - 1)
+    elif index < pivot:
+        return genes[index] + f_v(genes, pivot, index + 1)
---
+++ main.py
@@ -0,0 +1,10 @@
+from gsc import construct_seam
+
+if __name__ == "__main__":
+    c1 = (2, [0, -1, 2, 0])
+    c2 = (0, [3, 1, -1, 0])
+    c3 = (0, [2, -1, -1, -1])
+
+    print(construct_seam(c1))  # [(1, 1), (2, 1), (3, 2), (4, 2)]
+    print(construct_seam(c2))  # [(1, 3), (2, 4), (3, 3), (4, 3)]
+    print(construct_seam(c3))  # [(1, 2), (2, 1), (3, 0), (4, -1)] INVALID
