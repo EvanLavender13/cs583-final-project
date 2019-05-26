9480a6294908774ac46bc7268249e84b2dbfed9e
Evan Lavender <evanlavender@pop-os.localdomain>
2019-05-26 12:25:18 -0400

    Messing around with seam removal on small_tower image

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 from deap import base
 from deap import creator
 from deap import tools
 
-from gsc import get_energy_map, get_fitness
+from gsc import get_energy_map, get_fitness, construct_seam
 
 if __name__ == "__main__":
     image = sys.argv[1]
@@ -14,13 +15,15 @@ if __name__ == "__main__":
     image = cv2.imread(image, cv2.IMREAD_COLOR)
     grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    cv2.imwrite("grayscale.jpg", grayscale)
+
     ENERGY_MAP = get_energy_map(grayscale) / 255.0
     M = ENERGY_MAP.shape[0]
     PIVOT = np.random.randint(low=0, high=M)
 
     POP_SIZE = 10
-    CXPB = 0.5
-    MUTPB = 0.2
+    CXPB = 0.1
+    MUTPB = 0.05
 
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMax)
@@ -46,7 +49,15 @@ if __name__ == "__main__":
     for ind, fit in zip(population, fitnesses):
         ind.fitness.values = fit
 
-    NUM_GENS = 10
+    NUM_GENS = 25
+
+    stats = tools.Statistics(key=lambda indv: indv.fitness.values)
+    stats.register("avg", np.mean)
+    stats.register("std", np.std)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
+
+    logbook = tools.Logbook()
 
     for g in range(NUM_GENS):
 
@@ -76,8 +87,37 @@ if __name__ == "__main__":
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
-            print(ind[PIVOT])
             ind.fitness.values = fit
 
         # Replace population
         population[:] = offspring
+
+        record = stats.compile(population)
+        logbook.record(gen=g, **record)
+
+    for individual in population:
+        seam = construct_seam(PIVOT, individual)
+
+        m, n = image.shape[: 2]
+
+        output = np.zeros(shape=(m, n - 1, 3))
+
+        for row in range(m):
+            col = seam[row][1]
+            output[row, :, 0] = np.delete(image[row, :, 0], [col])
+            output[row, :, 1] = np.delete(image[row, :, 1], [col])
+            output[row, :, 2] = np.delete(image[row, :, 2], [col])
+
+        cv2.imwrite("seam_removed.jpg", output)
+
+        break
+
+    gen = logbook.select("gen")
+    fit_max = logbook.select("max")
+
+    fig, ax = plt.subplots()
+    line = ax.plot(gen, fit_max, "b-", label="Maximum Fitness")
+    ax.set_xlabel("Generation")
+    ax.set_ylabel("Fitness", color="b")
+
+    # plt.show()
