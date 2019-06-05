42d356c56f8e2d1ff78788f391044e88c0d950f7
Evan Lavender <evanlavender@pop-os.localdomain>
2019-06-05 19:35:24 -0400

    Oops

--- test/compare.py
+++ test/compare.py
@@ -1,11 +1,9 @@
 import logging
 import os
-
 import pickle
 
 import numpy as np
-
-from test.measure_image_similarity import structural_sim, pixel_sim, sift_sim, earth_movers_distance
+from measure_image_similarity import structural_sim, pixel_sim, sift_sim, earth_movers_distance
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
@@ -27,6 +25,8 @@ if __name__ == "__main__":
         avg_emd = []
 
         for i in range(10):
+            logging.info("Run #%s" % (i + 1))
+
             os.system("python3 ../main.py %s 263 316 %s %s 10 0.05 --display" % (input_image, OUTPUT_IMAGE, pop))
 
             avg_struct_sim.append(structural_sim(input_image, OUTPUT_IMAGE))
@@ -34,8 +34,9 @@ if __name__ == "__main__":
             avg_sif_sim.append(sift_sim(input_image, OUTPUT_IMAGE))
             avg_emd.append(earth_movers_distance(input_image, OUTPUT_IMAGE))
 
-        pop_data_dict[pop] = {"structural_similarity": np.mean(avg_struct_sim), "pixel_similarity": np.mean(avg_pix_sim),
-                          "sift_similarity": np.mean(avg_sif_sim), "earth_movers_distance": np.mean(avg_emd)}
+        pop_data_dict[pop] = {"structural_similarity": np.mean(avg_struct_sim),
+                              "pixel_similarity": np.mean(avg_pix_sim),
+                              "sift_similarity": np.mean(avg_sif_sim), "earth_movers_distance": np.mean(avg_emd)}
 
         print(pop, pop_data_dict[pop])
 
