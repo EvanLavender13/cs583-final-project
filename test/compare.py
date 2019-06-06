f201b2bceabec358d2874d21e14bdd98baab2ac2
Evan Lavender <evanlavender@pop-os.localdomain>
2019-06-05 22:58:24 -0400

    Added mutation prob stuff

--- test/compare.py
+++ test/compare.py
@@ -12,9 +12,11 @@ if __name__ == "__main__":
 
     pop_file = "pop_data.pickle"
     gen_file = "gen_data.pickle"
+    mut_file = "mut_data.pickle"
 
     pop_data_dict = {}
     gen_data_dict = {}
+    mut_data_dict = {}
 
     input_image = "../images/waterfall.jpg"
 
@@ -50,9 +52,9 @@ if __name__ == "__main__":
 
     input_image = "../images/whale.jpg"
 
-    logging.info("********** NUMBER OF GENERATIONS **********")
+    logging.info("********** MUTATION PROBABILITY **********")
 
-    if True:
+    if False:
         for gen in [5, 10, 25, 50, 100]:
             logging.info("***** %s Generations *****" % gen)
 
@@ -77,5 +79,33 @@ if __name__ == "__main__":
 
             print(gen, gen_data_dict[gen])
 
-        # Dump pop data
+        # Dump gen data
         pickle.dump(gen_data_dict, open(gen_file, "wb"))
+
+    if True:
+        for mut in [5, 10, 25, 50, 100]:
+            logging.info("***** Mutation Probability %s *****" % mut)
+
+            avg_struct_sim = []
+            avg_pix_sim = []
+            avg_sif_sim = []
+            avg_emd = []
+
+            for i in range(10):
+                logging.info("Run #%s" % (i + 1))
+
+                os.system("python3 ../main.py %s 340 408 %s 10 10 %s" % (input_image, OUTPUT_IMAGE, mut))
+
+                avg_struct_sim.append(structural_sim(input_image, OUTPUT_IMAGE))
+                avg_pix_sim.append(pixel_sim(input_image, OUTPUT_IMAGE))
+                avg_sif_sim.append(sift_sim(input_image, OUTPUT_IMAGE))
+                avg_emd.append(earth_movers_distance(input_image, OUTPUT_IMAGE))
+
+            gen_data_dict[gen] = {"structural_similarity": np.mean(avg_struct_sim),
+                                  "pixel_similarity": np.mean(avg_pix_sim),
+                                  "sift_similarity": np.mean(avg_sif_sim), "earth_movers_distance": np.mean(avg_emd)}
+
+            print(mut, mut_data_dict[mut])
+
+        # Dump gen data
+        pickle.dump(mut_data_dict, open(mut_file, "wb"))
